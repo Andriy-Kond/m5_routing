02daa2a3b78331cc95623fc7ea9b37b343f3e7f2
Andrii <andrii.kondratovich@gmail.com>
2024-10-12 14:38:03 +0300

    Updates

---
+++ static/js/326.3a243b7e.chunk.js
@@ -0,0 +1,63 @@
+"use strict";(self.webpackChunkm5_routing=self.webpackChunkm5_routing||[]).push([[326],{326:(e,n,t)=>{t.r(n),t.d(n,{default:()=>q});var s=t(216),i=t(43),r=t(475),o=t(119);const a=o.A.div`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 16px;
+`,l=o.A.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 8px 0;
+  margin-bottom: 16px;
+  border-bottom: 1px solid black;
+
+  > nav {
+    display: flex;
+  }
+`,d=o.A.p`
+  font-weight: 700;
+  margin: 0;
+`;(0,o.A)(r.k2)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: #000;
+  font-weight: 500;
+
+  transition: transform 0.3s ease;
+
+  &.active {
+    font-weight: bold;
+    color: #ff4500;
+  }
+
+  &.pending {
+    color: #0011ff;
+  }
+
+  &.transitioning {
+    transform: scale(1.15);
+  }
+`;var c=t(579);const h=(0,o.A)(r.k2)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: #000;
+  font-weight: 500;
+
+  transition: transform 0.3s ease;
+
+  &.active {
+    color: #ff4500;
+    background-color: #ff4500;
+  }
+
+  &.pending {
+    color: #0011ff;
+  }
+
+  &.transitioning {
+    transform: scale(1.15);
+  }
+`;const x=function(){return(0,c.jsxs)(a,{children:[(0,c.jsxs)(l,{children:[(0,c.jsxs)(d,{children:[(0,c.jsx)("span",{role:"img","aria-label":"computer icon",children:"\ud83d\udcbb"}),"GoMerch Store"]}),(0,c.jsxs)("nav",{children:[(0,c.jsx)(h,{to:"/navigation",end:!0,children:"Navigation Home"}),(0,c.jsx)(h,{to:"about",children:"About"}),(0,c.jsx)(h,{to:"products",children:"Products"})]})]}),(0,c.jsx)("main",{children:(0,c.jsx)(i.Suspense,{fallback:(0,c.jsx)("div",{children:"Loading..."}),children:(0,c.jsx)(s.sv,{})})}),(0,c.jsx)("footer",{children:"Footer"})]})};const p=function(){return(0,c.jsx)("h2",{children:"Gallery"})};const j=function(){return(0,c.jsx)("h2",{children:"Feedbacks"})},m=(0,i.lazy)((()=>t.e(299).then(t.bind(t,299)))),f=(0,i.lazy)((()=>t.e(854).then(t.bind(t,854)))),u=(0,i.lazy)((()=>t.e(106).then(t.bind(t,106)))),b=(0,i.lazy)((()=>Promise.all([t.e(926),t.e(1)]).then(t.bind(t,1)))),g=(0,i.lazy)((()=>t.e(466).then(t.bind(t,466)))),y=(0,i.lazy)((()=>t.e(826).then(t.bind(t,826)))),v=(0,i.lazy)((()=>t.e(485).then(t.bind(t,485)))),k=(0,i.lazy)((()=>t.e(759).then(t.bind(t,759))));const q=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)(s.BV,{children:[(0,c.jsxs)(s.qh,{path:"",element:(0,c.jsx)(x,{}),children:[(0,c.jsx)(s.qh,{index:!0,element:(0,c.jsx)(m,{})}),(0,c.jsxs)(s.qh,{path:"about",element:(0,c.jsx)(f,{}),children:[(0,c.jsx)(s.qh,{path:"mission",element:(0,c.jsx)(y,{})}),(0,c.jsx)(s.qh,{path:"team",element:(0,c.jsx)(v,{})}),(0,c.jsx)(s.qh,{path:"reviews",element:(0,c.jsx)(k,{})})]}),(0,c.jsx)(s.qh,{path:"products",element:(0,c.jsx)(u,{})}),(0,c.jsxs)(s.qh,{path:"products/:productId",element:(0,c.jsx)(b,{}),children:[(0,c.jsx)(s.qh,{path:"gallery",element:(0,c.jsx)(p,{})}),(0,c.jsx)(s.qh,{path:"feedbacks",element:(0,c.jsx)(j,{})})]})]}),(0,c.jsx)(s.qh,{path:"*",element:(0,c.jsx)(g,{})})]})})}}}]);
+//# sourceMappingURL=326.3a243b7e.chunk.js.map
